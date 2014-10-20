8ed0704613a72d22f569024f54f06f5919eb02ee
Yiyi Wang <shd101wyy@sina.com>
2014-10-20 17:10:42 -0500

    fix interface

--- administrator.php
+++ administrator.php
@@ -51,8 +51,14 @@
                     <input type="submit" data-icon="check" data-iconpos="right" data-inline="true" style="background:#2E64FE;" value="Save"> 
                 </form>
 
-                <a href="#" class="ui-btn ui-btn-inline ui-btn-b ui-shadow ui-corner-all ui-icon-check ui-btn-icon-left ui-btn-inline ui-mini ui-icon-delete" style="background:#DF0101;"> Delete </a>
+                <a href="#confirm_delete" class="ui-btn ui-btn-inline ui-btn-b ui-shadow ui-corner-all ui-icon-check ui-btn-icon-left ui-btn-inline ui-mini ui-icon-delete" style="background:#DF0101;" data-rel="popup"> Delete </a>
                 <a href="#" class="ui-btn ui-btn-inline ui-shadow ui-corner-all ui-btn-inline ui-mini" data-rel="back">Cancel</a>
+                
+                <div data-role="popup" id="confirm_delete">
+                    <p>Are u sure u want to delete this meal.</p>
+                    <a href="#confirm_delete" class="ui-btn ui-btn-inline ui-btn-b ui-shadow ui-corner-all ui-icon-check ui-btn-icon-left ui-btn-inline ui-mini ui-icon-delete" style="background:#DF0101;">Yes, Delete</a>
+                    <a href="#" class="ui-btn ui-btn-inline ui-shadow ui-corner-all ui-btn-inline ui-mini" data-rel="back"> No. </a>
+                </div>
             </div>
         </div>
         
@@ -115,7 +121,10 @@
             
             for(var i = 0; i < week_days.length; i++){
                 var li = "<li data-role='list-divider'> " + 
-                            "<p>" + week_days[i] + "</p>" + 
+                             "<div class='ui-grid-a'>" + 
+                                "<div class='ui-block-a' style='margin-top:10px;'><h3>" + week_days[i] + "</h3></div>" +
+                               "<div class='ui-block-b' style='text-align:right;'><a href='#' class='ui-btn ui-btn-inline ui-icon-plus ui-btn-icon-notext ui-corner-all ui-shadow'>add</a></div>" +
+                             "</div>" + 
                          "</li>";
                 $("#administrator_page_content ul").append(li);    // append to list
                 
@@ -133,7 +142,7 @@
                     var page_id = week_days[i]+j+"_"+Math.random();
                 
                     // show brief information of that meal
-                    var li = "<li> <a href='#meal_settings_panel'><p>" + intro + "</p></a>" +  
+                    var li = "<li> <a href='#meal_settings_panel' data-transition='slidefade'><p>" + intro + "</p></a>" +  
                              "</li>";
                     $("#administrator_page_content ul").append(li);
                     
